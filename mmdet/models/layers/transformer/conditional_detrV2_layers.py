8db35cabc0cdbcfe2ce2c0b5a1a78af021dee489
jasoni <jasoni4399@gmail.com>
2024-11-16 08:25:57 +0800

    Update conditional_detrV2_layers.py

--- mmdet/models/layers/transformer/conditional_detrV2_layers.py
+++ mmdet/models/layers/transformer/conditional_detrV2_layers.py
@@ -127,7 +127,7 @@ class ConditionalDetrTransformerV2Decoder(DetrTransformerDecoder):
         
         k=self.box_estimation(key_pos)
         pe=inverse_sigmoid(torch.cat([reference_xy, content_w_h],dim=2).permute(2,1,0)).permute(2,1,0)#
-        pe=pe[..., 2:]
+        pe=pe[..., :256]
         print(k.size(),pe.size())
         query=self.content_query(coordinate_to_encoding(coord_tensor=k+pe).sigmoid())
 
