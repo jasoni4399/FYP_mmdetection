233ce098ccbce62c2253c696b2ceca9e90a30cbb
jasoni <73955487+jasoni4399@users.noreply.github.com>
2025-02-24 01:16:13 +0800

    test conditional detr v1v2 resnet50dc5

--- configs/conditional_detr/conditional-detr_r50_8xb2-50e_coco.py
+++ configs/conditional_detr/conditional-detr_r50_8xb2-50e_coco.py
@@ -2,6 +2,19 @@ _base_ = ['../detr/detr_r50_8xb2-150e_coco.py']
 model = dict(
     type='ConditionalDETR',
     num_queries=300,
+    backbone=dict(
+    type='ResNet',
+    depth=50,
+    num_stages=4,
+    # Enable dilation in stage4 (last stage)
+    dilations=(1, 1, 1, 2),  
+    strides=(1, 2, 2, 1),    #Adjust strides to (1,2,2,1) for DC5
+    out_indices=(3,),
+    frozen_stages=1,
+    norm_cfg=dict(type='BN', requires_grad=False),
+    norm_eval=True,
+    style='pytorch',
+    init_cfg=dict(type='Pretrained', checkpoint='torchvision://resnet50')),
     decoder=dict(
         num_layers=6,
         layer_cfg=dict(
