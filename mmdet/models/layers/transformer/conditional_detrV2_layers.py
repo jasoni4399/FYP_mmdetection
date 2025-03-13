f3764a518ad05f46208f5a2a044e1bad54135a80
jasoni <73955487+jasoni4399@users.noreply.github.com>
2025-03-14 02:30:02 +0800

    Update conditional_detrV2_layers.py

--- mmdet/models/layers/transformer/conditional_detrV2_layers.py
+++ mmdet/models/layers/transformer/conditional_detrV2_layers.py
@@ -63,7 +63,7 @@ class ConditionalDetrTransformerV2Decoder(DetrTransformerDecoder):
         self.ref_point_head = MLP(self.embed_dims, self.embed_dims, 2, 2)
         self.lambda_q=MLP(self.embed_dims, self.embed_dims,self.embed_dims, 2)
 
-        self.ref_select_head=MLP(self.embed_dims, self.embed_dims,self.embed_dims, 2)
+        self.ref_select_head=MLP(self.embed_dims, self.embed_dims,2, 2)
         self.contentq_init=MLP(self.embed_dims, self.embed_dims,self.embed_dims, 2)
         #self.key_ref_select_head=MLP(self.embed_dims, self.embed_dims,2, 2)
         #self.content_query=MLP(self.embed_dims*2, self.embed_dims,
@@ -148,7 +148,7 @@ class ConditionalDetrTransformerV2Decoder(DetrTransformerDecoder):
 
                 #k=self.box_estimation(key_pos)
 
-                reference_selected=select(reference_unsigmoid,reference_xy,
+                reference_selected=select(reference_xy,reference_xy,
                                           bs,num_queries,self.embed_dims)
                 lambda_q_selected=select(lambda_q,reference_xy,
                                         bs,num_queries,self.embed_dims)
@@ -163,7 +163,7 @@ class ConditionalDetrTransformerV2Decoder(DetrTransformerDecoder):
                 reference_xy = selected_reference_sigmoid[...,:2]
 
                 pos_transformation = self.lambda_q_head(lambda_q_selected)
-                query=self.contentq_init(reference_selected)
+                query=self.contentq_init(key)
             else:
                 reference_unsigmoid=self.ref_point_head(query_pos)
                 reference_xy=reference_unsigmoid[...,:2]
